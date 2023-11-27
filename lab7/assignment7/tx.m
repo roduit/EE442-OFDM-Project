ef9d0603dcc7252e8bfbf968af07fe5bf5f98724
Filippo Quadri <filo1309@bluewin.ch>
2023-11-27 12:04:46 +0100

    solved size and receiver

--- lab7/assignment7/tx.m
+++ lab7/assignment7/tx.m
@@ -33,6 +33,9 @@ symbol_up = upsample(signal,conf.os_factor);
 
 filtered_tx_signal = matched_filter(symbol_up,conf.os_factor,conf.tx_filterlen);
 
+% Remove the non-information added by the convolution
+%filtered_tx_signal = filtered_tx_signal(1 + conf.tx_filterlen:end - conf.tx_filterlen);
+
 %Up conversion
 time = 0:1/conf.f_s:(length(filtered_tx_signal)/conf.f_s)-1/conf.f_s;
 txsignal = real(filtered_tx_signal) .* cos(2*pi*conf.f_c*time)' - imag(filtered_tx_signal) .* sin(2*pi*conf.f_c*time)';
