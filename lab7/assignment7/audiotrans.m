dd7ead2c758c2af41cf9f5339039eb7cf5f9a343
Vincent Roduit <101014161+roduit@users.noreply.github.com>
2023-11-28 08:46:04 +0100

    Modified for task 3

--- lab7/assignment7/audiotrans.m
+++ lab7/assignment7/audiotrans.m
@@ -52,94 +52,104 @@ res.rxnbits     = zeros(conf.nframes,1);
 
 
 % Results
-
-
-for k=1:conf.nframes
-    
-    % Generate random data
-    txbits = randi([0 1],conf.nbits,1);
-    
-    % TODO: Implement tx() Transmit Function
-    [txsignal conf] = tx(txbits,conf,k);
-    
-    % % % % % % % % % % % %
-    % Begin
-    % Audio Transmission
-    %
-    
-    % normalize values
-    peakvalue       = max(abs(txsignal));
-    normtxsignal    = txsignal / (peakvalue + 0.3);
-    
-    % create vector for transmission
-    rawtxsignal = [ zeros(conf.f_s,1) ; normtxsignal ;  zeros(conf.f_s,1) ]; % add padding before and after the signal
-    rawtxsignal = [  rawtxsignal  zeros(size(rawtxsignal)) ]; % add second channel: no signal
-    txdur       = length(rawtxsignal)/conf.f_s; % calculate length of transmitted signal
-    
-    % wavwrite(rawtxsignal,conf.f_s,16,'out.wav')   
-    audiowrite('out.wav',rawtxsignal,conf.f_s)  
-    
-    % Platform native audio mode 
-    if strcmp(conf.audiosystem,'native')
+freq_range = 100:100:2000;
+BER_list = zeros(size(freq_range));
+for ii = 1:numel(freq_range)
+    conf.f_sym = freq_range(ii);
+    for k=1:conf.nframes
         
-        % Windows WAV mode 
-        if ispc()
-            disp('Windows WAV');
-            wavplay(rawtxsignal,conf.f_s,'async');
+        % Generate random data
+        txbits = randi([0 1],conf.nbits,1);
+        
+        % TODO: Implement tx() Transmit Function
+        [txsignal conf] = tx(txbits,conf,k);
+        
+        % % % % % % % % % % % %
+        % Begin
+        % Audio Transmission
+        %
+        
+        % normalize values
+        peakvalue       = max(abs(txsignal));
+        normtxsignal    = txsignal / (peakvalue + 0.3);
+        
+        % create vector for transmission
+        rawtxsignal = [ zeros(conf.f_s,1) ; normtxsignal ;  zeros(conf.f_s,1) ]; % add padding before and after the signal
+        rawtxsignal = [  rawtxsignal  zeros(size(rawtxsignal)) ]; % add second channel: no signal
+        txdur       = length(rawtxsignal)/conf.f_s; % calculate length of transmitted signal
+        
+        % wavwrite(rawtxsignal,conf.f_s,16,'out.wav')   
+        audiowrite('out.wav',rawtxsignal,conf.f_s)  
+        
+        % Platform native audio mode 
+        if strcmp(conf.audiosystem,'native')
+            
+            % Windows WAV mode 
+            if ispc()
+                disp('Windows WAV');
+                wavplay(rawtxsignal,conf.f_s,'async');
+                disp('Recording in Progress');
+                rawrxsignal = wavrecord((txdur+1)*conf.f_s,conf.f_s);
+                disp('Recording complete')
+                rxsignal = rawrxsignal(1:end,1);
+    
+            % ALSA WAV mode 
+            elseif isunix()
+                disp('Linux ALSA');
+                cmd = sprintf('arecord -c 2 -r %d -f s16_le  -d %d in.wav &',conf.f_s,ceil(txdur)+1);
+                system(cmd); 
+                disp('Recording in Progress');
+                system('aplay  out.wav')
+                pause(2);
+                disp('Recording complete')
+                rawrxsignal = audioread('in.wav');
+                rxsignal    = rawrxsignal(1:end,1);
+            end
+            
+        % MATLAB audio mode
+        elseif strcmp(conf.audiosystem,'matlab')
+            disp('MATLAB generic');
+            playobj = audioplayer(rawtxsignal,conf.f_s,conf.bitsps);
+            recobj  = audiorecorder(conf.f_s,conf.bitsps,1);
+            record(recobj);
             disp('Recording in Progress');
-            rawrxsignal = wavrecord((txdur+1)*conf.f_s,conf.f_s);
+            playblocking(playobj)
+            pause(0.5);
+            stop(recobj);
             disp('Recording complete')
-            rxsignal = rawrxsignal(1:end,1);
-
-        % ALSA WAV mode 
-        elseif isunix()
-            disp('Linux ALSA');
-            cmd = sprintf('arecord -c 2 -r %d -f s16_le  -d %d in.wav &',conf.f_s,ceil(txdur)+1);
-            system(cmd); 
-            disp('Recording in Progress');
-            system('aplay  out.wav')
-            pause(2);
-            disp('Recording complete')
-            rawrxsignal = audioread('in.wav');
-            rxsignal    = rawrxsignal(1:end,1);
+            rawrxsignal  = getaudiodata(recobj,'int16');
+            rxsignal     = double(rawrxsignal(1:end))/double(intmax('int16')) ;
+            
+        elseif strcmp(conf.audiosystem,'bypass')
+            rawrxsignal = rawtxsignal(:,1);
+            rxsignal    = rawrxsignal;
         end
         
-    % MATLAB audio mode
-    elseif strcmp(conf.audiosystem,'matlab')
-        disp('MATLAB generic');
-        playobj = audioplayer(rawtxsignal,conf.f_s,conf.bitsps);
-        recobj  = audiorecorder(conf.f_s,conf.bitsps,1);
-        record(recobj);
-        disp('Recording in Progress');
-        playblocking(playobj)
-        pause(0.5);
-        stop(recobj);
-        disp('Recording complete')
-        rawrxsignal  = getaudiodata(recobj,'int16');
-        rxsignal     = double(rawrxsignal(1:end))/double(intmax('int16')) ;
+        % Plot received signal for debugging
+        % figure;
+        % plot(rxsignal);
+        % title('Received Signal')
+        % 
+        %
+        % End
+        % Audio Transmission   
+        % % % % % % % % % % % %
         
-    elseif strcmp(conf.audiosystem,'bypass')
-        rawrxsignal = rawtxsignal(:,1);
-        rxsignal    = rawrxsignal;
+        % TODO: Implement rx() Receive Function
+        [rxbits conf]       = rx(rxsignal,conf);
+        
+        res.rxnbits(k)      = length(rxbits);  
+        res.biterrors(k)    = sum(rxbits ~= txbits);
+        
     end
-    
-    % Plot received signal for debugging
-    figure;
-    plot(rxsignal);
-    title('Received Signal')
-    
-    %
-    % End
-    % Audio Transmission   
-    % % % % % % % % % % % %
-    
-    % TODO: Implement rx() Receive Function
-    [rxbits conf]       = rx(rxsignal,conf);
-    
-    res.rxnbits(k)      = length(rxbits);  
-    res.biterrors(k)    = sum(rxbits ~= txbits);
-    
+    per = sum(res.biterrors > 0)/conf.nframes;
+    ber = sum(res.biterrors)/sum(res.rxnbits);
+    BER_list(ii) = ber;
 end
 
-per = sum(res.biterrors > 0)/conf.nframes
-ber = sum(res.biterrors)/sum(res.rxnbits)
+figure;
+semilogy(freq_range, BER_list, 'bx-' ,'LineWidth',3)
+
+xlabel('Symbol rate')
+ylabel('BER')
+grid on
