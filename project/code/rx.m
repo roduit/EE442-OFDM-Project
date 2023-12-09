492c514d7bb16ddb89002da71505382b7854c16f
Filippo Quadri <filo1309@bluewin.ch>
2023-12-09 16:47:22 +0100

    phase and magnitude estimation

--- project/code/rx.m
+++ project/code/rx.m
@@ -2,10 +2,8 @@ function [rx_bitstream] = rx(rx_signal, conf)
     % rx_signal: received signal
     % conf: configuration struct
 
-    % For now we ignore the premable and the training sequence
-    % os_pream_length = conf.preamble_length*conf.os_factor_preamb + 2*conf.matched_filter_length_tx;
-
     training_seq = preamble_gen(conf.symb_per_packet);
+    training_seq = bit2bpsk(training_seq);
 
     os_training_length = conf.symb_per_packet * conf.os_factor_ofdm + conf.cp_length;
 
@@ -14,15 +12,11 @@ function [rx_bitstream] = rx(rx_signal, conf)
     r_dc = rx_signal .* exp(-1j*2*pi*conf.carrier_freq*time');
 
     % Low-pass filtering
-    rx_signal = 2 * ofdmlowpass(r_dc,conf,conf.BW * 1.5);
+    rx_signal = 2 * ofdmlowpass(r_dc,conf,conf.BW * 2);
 
     % Apply MF
-    %rx_signal_MF = rx_signal;
     rx_signal_MF = matched_filter(rx_signal, conf.os_factor_preamb, conf.matched_filter_length_rx, conf);
-    % rx_signal_MF = rx_signal_MF(1 + conf.matched_filter_length_rx: end - conf.matched_filter_length_rx);
 
-    % We need to remove the matched filter length because frame_sync gives us the data with padding
-    % beginning_of_data = frame_sync(rx_signal_MF, conf) - conf.matched_filter_length_rx;
     beginning_of_data = frame_sync(rx_signal_MF, conf);
 
     rx_signal_MF = rx_signal(beginning_of_data : beginning_of_data + os_training_length + conf.packet_per_frame * (conf.symb_per_packet * conf.os_factor_ofdm + conf.cp_length) - 1);
@@ -37,8 +31,16 @@ function [rx_bitstream] = rx(rx_signal, conf)
         rx_symbols(:, ii) = rx_fft;
 
     end
-    training_sequence_bitstream = bpsk2bit(rx_symbols(:, 1));
+    
+    % Extract the training sequence and compute the phase tracking
+    rx_training_sequence = rx_symbols(:, 1);
+    phase_estimation = mod(angle(rx_training_sequence ./ training_seq), 2*pi);
+    magnitude_estimation = abs(rx_training_sequence ./ training_seq);
 
+    training_seq_corrected = rx_training_sequence ./ magnitude_estimation .* exp(-1j*phase_estimation);
+
+    
+    % Extract the data symbol sequence and compute the bitstream
     rx_symbols_true = rx_symbols(:, 2:end);
     rx_bitstream = qpsk2bit(rx_symbols_true(:));
 
